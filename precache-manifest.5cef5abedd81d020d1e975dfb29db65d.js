70859d673048416ecc0a826a194d042f65952f5a
Setonji Avoseh <reachsetonji@gmail.com>
2020-09-29 17:18:35 +0100

    Updates

---
+++ precache-manifest.5cef5abedd81d020d1e975dfb29db65d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8bf1159fc262adb92ef01c69170c65",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a087179451c657a4de35",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "da7ce303b094ef6d7a00",
+    "url": "/robofriends/static/css/main.6538200c.chunk.css"
+  },
+  {
+    "revision": "a087179451c657a4de35",
+    "url": "/robofriends/static/js/2.e05fcfd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e05fcfd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7ce303b094ef6d7a00",
+    "url": "/robofriends/static/js/main.bb86b81c.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
